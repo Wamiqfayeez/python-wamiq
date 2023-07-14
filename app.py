03101bc8c5c89a2d62a80ceb89d1b8f9e20984b0
edris abdullah <ahmadedris1abdullah.study@gmail.com>
2023-07-14 19:04:11 +0500

    functionn

---
+++ app.py
@@ -0,0 +1,24 @@
+Character_Name = "Python "
+Character_Age = "33"
+print("Hi, my name is "  + Character_Name + " ") 
+print("I am " + Character_Age + " years old ")
+
+Character_First_Name = "Hello world "
+Character_Age = "69"
+print("Hi, my name is "  + Character_Name + "and I am " + Character_Age + " years old ") 
+
+Character_First_Name = "Hello "
+Character_Last_Name = "World "
+Character_Age = "96"
+Country = "afghanistan "
+Character_Full_Name = Character_First_Name + Character_Last_Name
+print("Hi, my name is "  + Character_Full_Name + " and I am " + Character_Age + " years old " + Country + " is where i was born")
+
+First_Name = "ahmad Edris"
+Last_Name = "Abdullah"
+Country = "afghanistan"
+Passport_No = "0000000"
+print(First_Name)
+print(Last_Name)
+print(Country)
+
---
+++ operations.py
@@ -0,0 +1,24 @@
+x = 2
+y = 2
+def plus():
+    print(x + y)
+
+def minus():
+    print(x - y)
+
+def multiply():
+    print(x * y)
+
+def difition():
+    print(x / y)
+
+def percentage():
+    print(x % y)
+
+
+plus()
+minus()
+multiply()
+difition()
+percentage()
+
